63024c0de806c297f25406f168b5065ab135756f
sarah-nolasco <Sarah.Nolasco@aviture.us.com>
2023-12-13 16:52:25 -0600

    Day 9

--- 2023-c#/AdventOfCode.Solutions/Year2023/Day09/Solution.cs
+++ 2023-c#/AdventOfCode.Solutions/Year2023/Day09/Solution.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Serialization;
+
 namespace AdventOfCode.Solutions.Year2023.Day09;
 
 class Solution : SolutionBase
@@ -8,12 +10,21 @@ class Solution : SolutionBase
     {
         var lines = Input.SplitByNewline();
 
+        long result = 0;
+
         foreach (string line in lines)
         {
-
+            Oasis oasis = new Oasis();
+            //Console.WriteLine("Processing: " + line);
+            oasis.StartingRow(line);
+            long step = oasis.Extrapolate();
+            result += step;
+            //Console.WriteLine("Return " + step);
+            //Console.WriteLine("-------------------------------");
         }
 
-        return "";
+
+        return result.ToString();
     }
 
     protected override string SolvePartTwo()
@@ -21,11 +32,105 @@ class Solution : SolutionBase
 
         var lines = Input.SplitByNewline();
 
+        long result = 0;
+
         foreach (string line in lines)
         {
-
+            Oasis oasis = new Oasis();
+            //Console.WriteLine("Processing: " + line);
+            oasis.StartingRow(line);
+            long step = oasis.ExtrapolateBackwards();
+            result += step;
+            //Console.WriteLine("Return " + step);
+            //Console.WriteLine("-------------------------------");
         }
 
-        return "";
+
+        return result.ToString();
     }
+
+    public class Oasis()
+    {
+        List<List<int>> History = new();
+
+
+        public void StartingRow(string line)
+        {
+            List<int> vals = new List<int>();
+            foreach (string val in line.Split(" "))
+            {
+                vals.Add(Int32.Parse(val));
+            }
+            History.Add(vals);
+            //Console.WriteLine("Read in starting row");
+        }
+
+        public long Extrapolate()
+        {
+            LoadRows();
+
+            long currTotal = 0;
+
+            for (int i = History.Count - 2; i >= 0; i--)
+            {
+                //Console.WriteLine("Adding " + History[i].Last());
+                currTotal += History[i].Last();
+            }
+
+
+            return currTotal;
+        }
+        public long ExtrapolateBackwards()
+        {
+            LoadRows();
+
+            long currTotal = 0;
+
+            for (int i = History.Count - 2; i >= 0; i--)
+            {
+                //Console.WriteLine("Adding " + History[i].Last());
+                currTotal = History[i][0] - currTotal;
+            }
+
+
+            return currTotal;
+        }
+
+
+        private void LoadRows()
+        {
+            while (!FoundEnd())
+            {
+                List<int> currRow = History.Last();
+
+                //Console.WriteLine("Looking for end " + String.Join("; ", currRow));
+
+                List<int> newRow = [];
+
+                for (int i = 0; i < (currRow.Count - 1); i++)
+                {
+                    newRow.Add(currRow[i + 1] - currRow[i]);
+                }
+                History.Add(newRow);
+
+            }
+        }
+
+        public Boolean FoundEnd()
+        {
+            foreach (int val in History.Last())
+            {
+                if (val != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true; ;
+        }
+
+
+    }
+
+
 }
