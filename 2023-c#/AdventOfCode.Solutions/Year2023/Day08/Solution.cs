432814c7130053724031f155642e78a72a9bd11a
sarah-nolasco <Sarah.Nolasco@aviture.us.com>
2023-12-13 15:25:27 -0600

    Day 11 - in a slump after 8 pt 2

--- 2023-c#/AdventOfCode.Solutions/Year2023/Day08/Solution.cs
+++ 2023-c#/AdventOfCode.Solutions/Year2023/Day08/Solution.cs
@@ -1,3 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+
 namespace AdventOfCode.Solutions.Year2023.Day08;
 
 class Solution : SolutionBase
@@ -6,26 +12,292 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
+        ElfMap map = new ElfMap();
         var lines = Input.SplitByNewline();
+        string currStep = "AAA";
+        string lastStep = "ZZZ";
 
         foreach (string line in lines)
         {
+            if (line.Contains("("))
+            {
+                //Console.WriteLine(line);
+                string node = line.Split("=")[0].Trim();
+                string next = line.Split("=")[1].Trim().Replace("(", "").Replace(")", "").Replace(",", "");
+                string left = next.Split(" ")[0].Trim();
+                string right = next.Split(" ")[1].Trim();
 
+                map.AddNode(node, left, right);
+                //Console.WriteLine(node + " = (" + left + ", " + right + ")");
+
+            }
+            else
+            {
+                //Console.WriteLine("Directions - " + line);
+                map.SetDirections(line.Trim());
+            }
         }
 
-        return "";
+        // this is the part that processes ------------------------------------
+
+        //Console.WriteLine("--------------------------------------------------------------------------------------");
+        //Console.WriteLine (new string(map.Directions));
+
+        Boolean complete = false;
+        int steps = 0;
+        //Console.WriteLine("Starting at " + currStep);
+
+        while (!complete)
+        {
+            foreach (char turn in map.Directions)
+            {
+                if (!complete)
+                {
+                    currStep = map.Step(turn, currStep);
+                    //Console.WriteLine("Turn " + turn + " to " + currStep);
+
+                    steps++;
+                    //Console.WriteLine(steps + "-------------");
+
+                    complete = (currStep.Equals(lastStep));
+                }
+
+            }
+        }
+
+
+        return steps.ToString();
     }
 
     protected override string SolvePartTwo()
     {
 
+        ElfMap map = new ElfMap();
         var lines = Input.SplitByNewline();
+        List<string> currStep = new List<string>();
 
         foreach (string line in lines)
         {
+            if (line.Contains("("))
+            {
+                string node = line.Split("=")[0].Trim();
+                string next = line.Split("=")[1].Trim().Replace("(", "").Replace(")", "").Replace(",", "");
+                string left = next.Split(" ")[0].Trim();
+                string right = next.Split(" ")[1].Trim();
+
+                map.AddNode(node, left, right);
+
+                if (node.Substring(2, 1).Equals("A"))
+                {
+                    currStep.Add(node);
+                }
+
+            }
+            else
+            {
+                map.SetDirections(line.Trim());
+            }
+        }
+
+        return CalculateSteps(currStep, map).ToString();
+    }
+
+    public long CalculateSteps(List<string> startVals, ElfMap map)
+    {
+
+        Console.WriteLine("Checking for loops");
+        Loops loops = new Loops(startVals.Count);
+
+        for (int i = 0; i < startVals.Count; i++)
+        {
+            int[] currLoop = CalculateLoop(startVals[i], map);
+            loops.starts[i] = currLoop[0];
+            loops.loopSteps[i] = currLoop[1];
+        }
+
+        loops.Start();
+        Boolean found = false;
+        long iteration = 100000000;
+        long count = 0;
+        long passes = 0;
+
+        while (!found)
+        {
+            found = loops.Step();
+            if (count % iteration == 0)
+            {
+                count = 0;
+                Console.WriteLine(passes + " - 100M passes: " + " - " + loops.currStep.Max());
+                passes++;
+                if (loops.currStep.Max() > 20131447079522)
+                {
+                    Console.WriteLine("WRONG");
+                    found = true;
+                }
+            }
+            count++;
+        }
+
+        return 0;
+    }
+    // too high 38585277158834
+    //          30197172967610
+    //          20131447079522
+    //
+    // -----------------------
+    // low       3355237900838
+
+
+
+    private int[] CalculateLoop(string start, ElfMap map)
+    {
+
+        Boolean complete = false;
+        int index = -1;
+        int steps = -1;
+        int loopStart = -1;
+        int loopSteps = -1;
+
+        //Console.WriteLine("Inside loop check - " + start);
+        //Console.WriteLine("------------------");
+
+        string currStep = start;
+        Boolean loopFound = false;
+
+        while (!complete)
+        {
+            foreach (char turn in map.Directions)
+            {
+                if (!complete)
+                {
+                    complete = currStep.Substring(2, 1).Equals("Z");
+
+                    currStep = map.Step(turn, currStep);
+                    //Console.WriteLine(currStep);
+                    steps++;
+                    index++;
+
+                    // populate loop start
+                    if (loopStart == -1)
+                    {
+                        loopStart = index;
+                    }
+
+                    else if (complete && !loopFound)
+                    {
+                        loopFound = true;
+                        loopSteps = steps;
+                        steps = -1;
+                        //Console.WriteLine("Found a end at " + loopStart + " with " + loopSteps + " steps");
+                        complete = false;
+
+                    }
+                    else if (complete && loopFound)
+                    {
+                        if (!(steps == loopSteps))
+                        {
+                            //Console.WriteLine("Loop Thrash old steps - " + loopSteps + " new - " + steps);
+                            loopStart = index;
+                            complete = false;
+                            loopSteps = steps;
+                            steps = -1;
+
+                        }
+                        else
+                        {
+                            //Console.WriteLine("SUCCESS");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        //Console.WriteLine("------------------\n");
+        return [loopStart, loopSteps];
+    }
+
+
+    public class ElfMap
+    {
+        public char[] Directions;
+        public Dictionary<string, string[]> Nodes;
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public ElfMap()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+
+            Nodes = new Dictionary<string, string[]>();
+        }
+
+        public void SetDirections(string directions)
+        {
+            Directions = directions.ToCharArray();
+        }
+        public void AddNode(string node, string left, string right)
+        {
+            Nodes.Add(node, [left, right]);
+        }
+
+        public string Step(char turn, String node)
+        {
+            //Console.WriteLine(node + " = (" + Nodes[node][0] + ", " + Nodes[node][1] + ")");
+
+            if (turn == 'L')
+            {
+                return Nodes[node][0];
+
+            }
+            else if (turn == 'R')
+            {
+                return Nodes[node][1];
+
+            }
+            return "";
 
         }
 
-        return "";
     }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public class Loops(int size)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+        public long[] starts = new long[size];
+        public long[] loopSteps = new long[size];
+        public long[] currStep = new long[size];
+
+        public void Start()
+        {
+            currStep = starts;
+        }
+
+        public Boolean Step()
+        {
+
+
+            for (int i = 0; i < starts.Length; i++)
+            {
+                while (currStep[i] < currStep[(i + 1) % starts.Length])
+                {
+                    currStep[i] += loopSteps[i];
+                }
+            }
+
+            for (int i = 1; i < starts.Length; i++)
+            {
+                if (currStep[i] != currStep[i - 1])
+                {
+                    return false;
+                }
+
+            }
+
+
+            return true;
+
+        }
+
+    }
+
 }
--- 2023-c#/AdventOfCode.Solutions/Year2023/Day11/Solution.cs
+++ 2023-c#/AdventOfCode.Solutions/Year2023/Day11/Solution.cs
@@ -1,3 +1,6 @@
+using System.Reflection.Emit;
+using System.Runtime.ExceptionServices;
+
 namespace AdventOfCode.Solutions.Year2023.Day11;
 
 class Solution : SolutionBase
@@ -8,12 +11,63 @@ class Solution : SolutionBase
     {
         var lines = Input.SplitByNewline();
 
+        // list of empty columns
+        // list of empty rows
+        List<long> emptyRows = new List<long>();
+        List<long> emptyCols = new List<long>();
+        List<Galaxy> universe = new List<Galaxy>();
+        long rowIndex = 0;
+
+
         foreach (string line in lines)
         {
+            if (!line.Contains("#"))
+            {
+                emptyRows.Add(rowIndex);
+            }
+            long colIndex = 0;
+            if (emptyCols.Count() == 0)
+            {
+                foreach (char letter in line)
+                {
+                    emptyCols.Add(colIndex);
+                    colIndex++;
+                }
+            }
 
+            colIndex = 0;
+            foreach (char letter in line)
+            {
+                if (!letter.Equals('.'))
+                {
+                    universe.Add(new Galaxy(rowIndex, colIndex));
+                    emptyCols.Remove(colIndex);
+                }
+
+                colIndex++;
+            }
+
+            rowIndex++;
         }
 
-        return "";
+
+        // expand the universe
+        foreach (Galaxy galaxy in universe)
+        {
+            galaxy.Expand(emptyRows, emptyCols, 1);
+        }
+
+        // calculate paths
+        long length = 0;
+        for (int index = 0; index < universe.Count; index++)
+        {
+            for (int compare = (index + 1); compare < universe.Count(); compare++)
+            {
+                length += universe[index].Navigate(universe[compare]);
+            }
+        }
+
+        return length.ToString();
     }
 
     protected override string SolvePartTwo()
@@ -21,11 +75,133 @@ class Solution : SolutionBase
 
         var lines = Input.SplitByNewline();
 
+        // list of empty columns
+        // list of empty rows
+        List<long> emptyRows = new List<long>();
+        List<long> emptyCols = new List<long>();
+        List<Galaxy> universe = new List<Galaxy>();
+        long rowIndex = 0;
+
+
         foreach (string line in lines)
         {
+            if (!line.Contains("#"))
+            {
+                emptyRows.Add(rowIndex);
+            }
+
+
+            // load emptycols
+
+            long colIndex = 0;
+
+
+            if (emptyCols.Count() == 0)
+            {
+                foreach (char letter in line)
+                {
+                    emptyCols.Add(colIndex);
+                    colIndex++;
+                }
+            }
+
+            colIndex = 0;
+            foreach (char letter in line)
+            {
+                if (!letter.Equals('.'))
+                {
+                    universe.Add(new Galaxy(rowIndex, colIndex));
+                    emptyCols.Remove(colIndex);
+                }
+
+                colIndex++;
+            }
+
+            rowIndex++;
+        }
+
+
+        /*
+        // print empty
+        Console.Write("Empty rows: ");
+
+        foreach (int emptyRow in emptyRows)
+        {
+            Console.Write(emptyRow + ", ");
 
         }
 
-        return "";
+        Console.WriteLine();
+        Console.Write("Empty cols: ");
+
+        foreach (int emptyCol in emptyCols)
+        {
+            Console.Write(emptyCol + ", ");
+        }
+        Console.WriteLine();
+        */
+
+        // expand the universe
+        foreach (Galaxy galaxy in universe)
+        {
+            //Console.Write("Moving [" + galaxy.X + ", " + galaxy.Y + "] -> [");
+            galaxy.Expand(emptyRows, emptyCols, (1000000 - 1));
+            //Console.WriteLine(galaxy.X + ", " + galaxy.Y + "]");
+        }
+
+        // calculate paths
+        long length = 0;
+        for (int index = 0; index < universe.Count; index++)
+        {
+            for (int compare = index + 1; compare < universe.Count; compare++)
+            {
+                length += universe[index].Navigate(universe[compare]);
+            }
+        }
+
+        return length.ToString();
+    }
+
+    //827010736819 <- too high
+
+    private class Galaxy(long x, long y)
+    {
+        public long X = x;
+        public long Y = y;
+
+        public void Expand(List<long> rows, List<long> cols, long expanse)
+        {
+            long rowOffset = 0;
+            long colOffset = 0;
+            foreach (long row in rows)
+            {
+                if (row < X)
+                {
+                    rowOffset++;
+                }
+            }
+            foreach (long col in cols)
+            {
+                if (col < Y)
+                {
+                    colOffset++;
+                }
+            }
+
+            rowOffset *= expanse;
+            colOffset *= expanse;
+
+            X += rowOffset;
+            Y += colOffset;
+
+        }
+
+        public long Navigate(Galaxy galaxy)
+        {
+            long distance = Math.Abs((X - galaxy.X)) + Math.Abs((Y - galaxy.Y));
+            //Console.WriteLine("[" + X + ", " + Y + "] -> [" + galaxy.X + ", " + galaxy.Y + "] = " + distance);
+            return distance;
+        }
+
     }
 }
