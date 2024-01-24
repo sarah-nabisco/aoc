8a3713fe71c095aae1a77b52dc6f4a93f0e43ee7
sarah-nolasco <Sarah.Nolasco@aviture.us.com>
2024-01-24 16:17:00 -0600

    Day 15 pt 1

--- 2023-c#/AdventOfCode.Solutions/Year2023/Day15/Solution.cs
+++ 2023-c#/AdventOfCode.Solutions/Year2023/Day15/Solution.cs
@@ -6,14 +6,33 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
-        var lines = Input.SplitByNewline();
+        List<string> inputs = [.. Input.SplitByNewline()[0].Split(",")];
+        long results = 0;
 
-        foreach (string line in lines)
+        foreach (string input in inputs)
         {
-
+            git results += ParseHash(input);
         }
 
-        return "";
+        return results.ToString();
+    }
+
+    private int ParseHash(string input)
+    {
+        int result = 0;
+
+        foreach (char curr in input)
+        {
+            //Increase the current value by the ASCII code for the current character of the string
+            result += ((int)curr);
+            //Set the current value to itself multiplied by 17.
+            result *= 17;
+            //Set the current value to the remainder of dividing itself by 256.
+            result %= 256;
+        }
+
+
+        return result;
     }
 
     protected override string SolvePartTwo()
