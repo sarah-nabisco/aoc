83fa6104623ceede7e364eccb853590291c38a21
sarah-nolasco <Sarah.Nolasco@aviture.us.com>
2024-01-25 13:53:44 -0600

    Day 15 pt 2

--- 2023-c#/AdventOfCode.Solutions/Year2023/Day15/Solution.cs
+++ 2023-c#/AdventOfCode.Solutions/Year2023/Day15/Solution.cs
@@ -38,13 +38,200 @@ class Solution : SolutionBase
     protected override string SolvePartTwo()
     {
 
-        var lines = Input.SplitByNewline();
+        List<string> inputs = [.. Input.SplitByNewline()[0].Split(",")];
+        Boxes boxes = new Boxes();
 
-        foreach (string line in lines)
+        foreach (string input in inputs)
         {
+            Lens lens = new Lens(input);
+            Console.WriteLine(input + " = " + lens.label + "|" + lens.focalLength + "|" + lens.hasDash + " -> " + lens.boxIndex);
+            boxes.AddLens(lens);
+        }
+        //boxes.Print();
+
+
+
+
+        return boxes.TotalFocus().ToString();
+    }
+    public class Lens
+    {
+        public string input;
+        public string label;
+        public int focalLength;
+        public bool hasDash;
+        public int boxIndex;
+
+        public Lens(string a)
+        {
+            input = a;
+            hasDash = true;
+
+            int index = input.IndexOf('-');
+
+            if (index == -1)
+            {
+                index = input.IndexOf('=');
+                hasDash = false;
+            }
+            if (index == -1)
+            {
+                Console.WriteLine("WE HAVE A PROBELM WITH INPUT: " + input + "----------------\n------------------\n---------------------");
+            }
+
+            label = input.Substring(0, index);
+
+            if (!hasDash)
+            {
+                //Console.WriteLine("Parse " + input[(index + 1)..] + " -> " + index + " | " + hasDash + " | " + input);
+                focalLength = int.Parse(input[(index + 1)..]);
+            }
+
+            boxIndex = 0;
+            foreach (char curr in label)
+            {
+                //Increase the current value by the ASCII code for the current character of the string
+                boxIndex += ((int)curr);
+                //Set the current value to itself multiplied by 17.
+                boxIndex *= 17;
+                //Set the current value to the remainder of dividing itself by 256.
+                boxIndex %= 256;
+            }
 
         }
 
-        return "";
     }
+    public class Boxes
+    {
+        public List<Lens>[] boxes;
+
+        public Boxes()
+        {
+            boxes = new List<Lens>[256];
+            for (int i = 0; i < 256; i++)
+            {
+                boxes[i] = [];
+            }
+        }
+
+        public void AddLens(Lens lens)
+        {
+
+            List<Lens> currBox = boxes[lens.boxIndex];
+            int index = ContainsLable(lens, currBox);
+
+            if (index != -1)
+            {
+                currBox.RemoveAt(index);
+
+            }
+            // DASH
+            // ----check if box includes lens with label
+            // ----if it does remove it
+
+            // EQUALS
+            // ----check if box includes a lens with label
+            // ----     yes - swap out with new lens
+            // ----     no -  add to back of box
+
+
+            if (!lens.hasDash)
+            {
+                if (index != -1)
+                {
+                    currBox.Insert(index, lens);
+                }
+                else
+                {
+                    currBox.Add(lens);
+                }
+
+            }
+
+            boxes[lens.boxIndex] = currBox;
+
+
+        }
+
+        private int ContainsLable(Lens lens, List<Lens> box)
+        {
+            int index = 0;
+            bool found = false;
+
+            foreach (Lens boxLens in box)
+            {
+                if (!found)
+                {
+                    if (boxLens.label.Equals(lens.label))
+                    {
+                        found = true;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+
+                }
+
+            }
+
+            return found ? index : -1;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("--------------------------------------");
+            int index = 0;
+            foreach (List<Lens> box in boxes)
+            {
+                if (box.Count > 0)
+                {
+                    Console.Write("Box " + index + ": ");
+
+                    foreach (Lens lens in box)
+                    {
+                        Console.Write("[" + lens.label + " " + lens.focalLength + "] ");
+                    }
+                    Console.Write("\n");
+                }
+
+                index++;
+            }
+            Console.WriteLine("--------------------------------------");
+        }
+
+        public long TotalFocus()
+        {
+            int boxIndex = 0;
+            long totalFocus = 0;
+
+            foreach (List<Lens> box in boxes)
+            {
+                if (box.Count > 0)
+                {
+                    int lensIndex = 1;
+
+                    foreach (Lens lens in box)
+                    {
+                        long lensFocus = 1 + boxIndex;
+                        lensFocus *= lensIndex;
+                        lensFocus *= lens.focalLength;
+
+                        totalFocus += lensFocus;
+                        //Console.WriteLine("Box " + boxIndex + " - " + lens.label + " " + lens.focalLength + " | [boxindex " + (boxIndex + 1) + "] * [slot " + lensIndex + "] * [focal " + lens.focalLength + "] -> " + lensFocus);
+
+
+                        lensIndex++;
+                    }
+
+                }
+
+                boxIndex++;
+            }
+
+            return totalFocus;
+        }
+    }
+
+
 }
