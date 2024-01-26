d1809f96ce7e8eeb4482634bf715bf5443dc93da
sarah-nolasco <Sarah.Nolasco@aviture.us.com>
2024-01-26 17:46:23 -0600

    day 14

--- 2023-c#/AdventOfCode.Solutions/Year2023/Day14/Solution.cs
+++ 2023-c#/AdventOfCode.Solutions/Year2023/Day14/Solution.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+
 namespace AdventOfCode.Solutions.Year2023.Day14;
 
 class Solution : SolutionBase
@@ -7,13 +10,16 @@ class Solution : SolutionBase
     protected override string SolvePartOne()
     {
         var lines = Input.SplitByNewline();
+        Platform platform = new Platform();
 
         foreach (string line in lines)
         {
-
+            Console.WriteLine("Reading [" + line + ']');
+            platform.Add(line);
         }
+        platform.PrintPlatform();
 
-        return "";
+        return platform.CalculateLoad().ToString();
     }
 
     protected override string SolvePartTwo()
@@ -28,4 +34,123 @@ class Solution : SolutionBase
 
         return "";
     }
+
+    public class Platform
+    {
+        private List<char[]> platform;
+        public Platform()
+        {
+            platform = [];
+
+        }
+
+        public void Add(string row)
+        {
+            row = row.Replace('.', ' ');
+            if (platform.Count == 0)
+            {
+                platform.Add(row.ToCharArray());
+            }
+            else
+            {
+
+                char[] currRow = new char[row.Length];
+                for (int i = 0; i < currRow.Length; i++)
+                {
+                    currRow[i] = ' ';
+                }
+
+
+                List<int> ball = [];
+
+                for (int index = 0; index < row.Length; index++)
+                {
+                    char curr = row[index];
+                    if (curr.Equals('O'))
+                    {
+                        ball.Add(index);
+                    }
+                    else if (curr.Equals('#'))
+                    {
+                        currRow[index] = '#';
+                    }
+                }
+                platform.Add(currRow);
+                AddRound(ball);
+            }
+
+
+
+        }
+
+        private void AddRound(List<int> balls)
+        {
+            foreach (int ball in balls)
+            {
+                bool dropped = false;
+
+                // starting at the top
+                int rowIndex = platform.Count - 1;
+
+                while (!dropped)
+                {
+                    char[] curr = platform[rowIndex];
+
+                    if (rowIndex == 0)
+                    {
+                        curr[ball] = 'O';
+                        dropped = true;
+                        platform[rowIndex] = curr;
+                    }
+                    else
+                    {
+                        char nextVal = platform[rowIndex - 1][ball];
+
+                        if (nextVal.Equals('#') || nextVal.Equals('O'))
+                        {
+                            curr[ball] = 'O';
+                            dropped = true;
+                            platform[rowIndex] = curr;
+                        }
+                    }
+                    rowIndex--;
+
+                }
+            }
+
+        }
+        public void PrintPlatform()
+        {
+            foreach (char[] row in platform)
+            {
+                Console.WriteLine(new string(row));
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("--------------------------------------------------\n\n");
+        }
+
+        public long CalculateLoad()
+        {
+            long total = 0;
+
+            for (int index = 0; index < platform.Count; index++)
+            {
+                foreach (char val in platform[index])
+                {
+
+                    if (val.Equals('O'))
+                    {
+                        total += (long)(platform.Count - index);
+                    }
+                }
+
+            }
+
+            return total;
+
+        }
+    }
+
+
 }
